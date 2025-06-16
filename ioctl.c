ffdb6095b3b532924e0e917bb328e98c6c7c49f2
Vivek Patidar <vpatidar@purestorage.com>
2025-06-16 09:21:40 +0000

    Fix kiocb_set_rw_flags. linux commit c34fc6f26ab86d03a2d47446f42b6cd492dfdc56
    Signed-off-by: Vivek Patidar <vpatidar@purestorage.com>

--- ioctl.c
+++ ioctl.c
@@ -5409,7 +5409,7 @@ static int btrfs_ioctl_encoded_write(struct file *file, void __user *argp, bool 
 		goto out_end_write;
 
 	init_sync_kiocb(&kiocb, file);
-	ret = kiocb_set_rw_flags(&kiocb, 0);
+	ret = kiocb_set_rw_flags(&kiocb, 0, WRITE);
 	if (ret)
 		goto out_end_write;
 	kiocb.ki_pos = pos;
